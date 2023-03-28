bee0f21fb9c3c1e5f5de7ddc177f86716ea8e676
阿古 <ken20221031@gmail.com>
2023-03-28 12:08:23 +0800

    新增 商品api

---
+++ app/Controller/Api/ProductController.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use Hyperf\HttpServer\Contract\RequestInterface;
+use Hyperf\HttpServer\Contract\ResponseInterface;
+use App\Constants\ErrorCode;
+use App\Controller\AbstractController;
+use App\Service\ProductService;
+use Hyperf\HttpServer\Annotation\Controller;
+use Hyperf\HttpServer\Annotation\RequestMapping;
+use App\Request\ProductApiRequest;
+
+/**
+ * @Controller
+ */
+class ProductController extends AbstractController
+{
+    /**
+     * @RequestMapping(path="list", methods="POST")
+     * 獲取商品列表 
+     */
+    public function list(ProductApiRequest $request, ProductService $service)
+    {
+        $keyword = $request->input('keyword','');
+        $offset = $request->input('offset', 0);
+        $limit = $request->input('limit', 0);
+        $result = $service->getListByKeyword($keyword, $offset,  $limit);
+        return $this->success($result);
+    }
+
+    /**
+     * @RequestMapping(path="count", methods="get")
+     * 獲取上架中的商品數
+     */
+    public function count(ProductService $service)
+    {
+        $result = $service->getCount();
+        $data = array(
+            'count' => $result
+        );
+        return $this->success($data);
+    }
+}
