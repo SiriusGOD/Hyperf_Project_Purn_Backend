0eae1a7e642817feff7a8d1c8e412b3515daf6a0
victor20220919 <victor20220919@gmail.com>
2023-03-28 18:21:34 +0800

    upgrade video column

--- app/Model/Video.php
+++ app/Model/Video.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types=1);
 /**
  * This file is part of Hyperf.
  *
@@ -13,52 +13,36 @@ namespace App\Model;
 
 use Carbon\Carbon;
 use Hyperf\Database\Model\SoftDeletes;
-
 /**
- * @property int $id
- * @property int $user_id
- * @property string $name
- * @property string $thumbnail
- * @property string $url
- * @property int $length
- * @property int $likes
- * @property string $refreshed_at
- * @property Carbon $created_at
- * @property Carbon $updated_at
- * @property Carbon $deleted_at
+ * @property-read User $user 
+ * @property-read \Hyperf\Database\Model\Collection|Tag[] $tags 
  */
 class Video extends Model
 {
     use SoftDeletes;
-
     public const PAGE_PER = 10;
-
     /**
      * The table associated with the model.
      *
      * @var string
      */
     protected $table = 'videos';
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [];
-
     /**
      * The attributes that should be cast to native types.
      *
      * @var array
      */
-    protected $casts = ['id' => 'integer', 'user_id' => 'integer', 'length' => 'integer', 'likes' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
-
+    protected $casts = [];
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-
     public function tags()
     {
         return $this->morphToMany(Tag::class, 'correspond', 'tag_corresponds', 'correspond_id');
