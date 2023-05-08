63ce932f2c8d3132e67b115f8c5ca79fb40b02d0
“tw0645” <chenyutim635@gmail.com>
2023-05-08 18:49:00 +0800

    Revert "修正付款顯示"
    This reverts commit 0b8f39779fa6e55d384d9a82ea7f76b472a7f238.

--- app/Controller/Api/ImageGroupController.php
+++ app/Controller/Api/ImageGroupController.php
@@ -114,9 +114,7 @@ class ImageGroupController extends AbstractController
         $memberId = auth()->user()->getId();
 
         if (! $service->isPay($id, $memberId)) {
-            $this->success([
-                'is_pay' => 0,
-            ]);
+            return $this->error(trans('validation.is_not_pay'), 400);
         }
 
         return $this->success([
