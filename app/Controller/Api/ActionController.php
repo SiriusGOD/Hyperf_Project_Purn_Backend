c5f4364dd8dfad909e9c080eb98944f53ecae9ca
阿古 <ken20221031@gmail.com>
2023-05-08 16:03:36 +0800

    修改 獲取檢舉選項Api

--- app/Controller/Api/ActionController.php
+++ app/Controller/Api/ActionController.php
@@ -17,22 +17,7 @@ class ActionController extends AbstractController
     #[RequestMapping(methods: ['POST'], path: 'getReportItem')]
     public function getReportItem(RequestInterface $request)
     {
-        $data = [array(
-                    'id' => 1,
-                    'item' => '播放卡頓'
-                ),array(
-                    'id' => 2,
-                    'item' => '我認為不該出現在這'
-                ),array(
-                    'id' => 3,
-                    'item' => '內容血腥'
-                ),array(
-                    'id' => 4,
-                    'item' => '內容噁心'
-                ),array(
-                    'id' => 5,
-                    'item' => '長得太醜'
-                )];
+        $data = ['播放卡頓','我認為不該出現在這','內容血腥','內容噁心','長得太醜'];
         return $this->success(['models' => $data]);
     }
 
