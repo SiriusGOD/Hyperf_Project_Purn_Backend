558ba50ae5c21315c982f1ee4e9890732b27d353
阿古 <ken20221031@gmail.com>
2023-05-22 17:25:30 +0800

    fix bug

--- migrations/2023_05_22_110347_create_drive_group_has_class.php
+++ migrations/2023_05_22_110347_create_drive_group_has_class.php
@@ -4,7 +4,7 @@ use Hyperf\Database\Schema\Schema;
 use Hyperf\Database\Schema\Blueprint;
 use Hyperf\Database\Migrations\Migration;
 
-class CreateDriveGroupHasGroups extends Migration
+class CreateDriveGroupHasClass extends Migration
 {
     /**
      * Run the migrations.
