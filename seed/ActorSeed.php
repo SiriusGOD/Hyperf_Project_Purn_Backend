0daf087746f683abfc9879914e8e6299a76624c5
阿古 <ken20221031@gmail.com>
2023-05-23 15:07:55 +0800

    新增 演員Seed

---
+++ seed/ActorSeed.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Model\Actor;
+use App\Model\ActorHasClassification;
+
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+class ActorSeed implements BaseInterface
+{
+    public function up(): void
+    {
+        // 讀檔
+        $filePath = './public/actor/actors.csv'; // 檔案路徑
+
+        $file = fopen($filePath, 'r');
+
+        if ($file) {
+            $key = 0;
+            // 清空 actor table
+            Actor::truncate();
+            // 清空 actor_has_classifications
+            ActorHasClassification::truncate();
+            while (($line = fgets($file)) !== false) {
+                if($key == 0){
+                    $key++;
+                    continue;
+                }
+                $parts = explode(',', $line);
+                var_dump($parts);
+                
+                // insert to actor table
+                $model = new Actor();
+                $model -> user_id = 1;
+                $model -> sex = 1;
+                $model -> name = trim($parts[0]);
+                $model -> save();
+                
+                // insert to actor_has_classifications
+                $ahc = new ActorHasClassification();
+                $ahc -> actor_id = $model -> id;
+                $ahc -> actor_classifications_id = (int)trim($parts[1]);
+                $ahc -> save();
+                
+                $key++;
+                var_dump('已新增第'.$key.'筆');
+            }
+            
+            fclose($file);
+        }
+    }
+
+    public function down(): void
+    {
+        \App\Model\ActorClassification::truncate();
+    }
+
+    public function base(): bool
+    {
+        return false;
+    }
+}
